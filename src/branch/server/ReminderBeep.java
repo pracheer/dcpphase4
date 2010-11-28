cf3a65d47c2c2dbc1d160318d5ffe1644221cffa
kejriwalnikhil@gmail.com <kejriwalnikhil@gmail.com@49332bda-17ce-de00-fbf4-6a5fc677c1da>
2010-11-28 08:29:48 +0000

    FDSensor

---
+++ src/branch/server/ReminderBeep.java
@@ -0,0 +1,43 @@
+package branch.server;
+
+import java.awt.Toolkit;
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Simple demo that uses java.util.Timer to schedule a task to execute once 5
+ * seconds have passed.
+ */
+
+public class ReminderBeep {
+  Toolkit toolkit;
+
+  Timer timer;
+
+  int counter = 3;
+  
+  public ReminderBeep(int seconds) {
+    toolkit = Toolkit.getDefaultToolkit();
+    timer = new Timer();
+    timer.schedule(new RemindTask(), seconds * 1000);
+  }
+
+  class RemindTask extends TimerTask {
+    public void run() {
+      System.out.println("Time's up!");
+      toolkit.beep();
+      //timer.cancel(); //Not necessary because we call System.exit
+//      System.exit(0); //Stops the AWT thread (and everything else)
+      if(--counter > 0)
+    	  timer.schedule(new RemindTask(), 5000);
+      else
+    	  System.exit(0);
+    }
+  }
+
+  public static void main(String args[]) {
+    System.out.println("About to schedule task.");
+    new ReminderBeep(5);
+    System.out.println("Task scheduled.");
+  }
+}
---
+++ src/test/FDsensTest.java
@@ -0,0 +1,81 @@
+package test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Timer;
+import java.util.Vector;
+
+import branch.server.FDsens;
+import branch.server.FDsens.AliveMsg;
+import branch.server.FDsens.ListeningThread;
+import junit.framework.TestCase;
+
+public class FDsensTest extends TestCase {
+	private static int default_timeout = 10000;
+	private static int timeoutInc = 10;
+	public static int pingtime = default_timeout - timeoutInc;
+	private static String msgSeparator = "::";
+	static Timer alivetimer = new Timer();
+	HashMap<String, Timer> timers;
+	FDsens sensor = new FDsens();
+	
+
+	public FDsensTest(String name) {
+		super(name);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+		
+		try {
+			//alivetimer.schedule(sensor.new AliveMsg(), pingtime);
+			sensor.new ListeningThread().start();
+			System.out.println("main over");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+	
+	public final void testFDsens() {
+		try {
+			Socket destSocket = null;
+			String str;
+			
+			Thread.sleep(4000);
+			destSocket = new Socket("localhost", 10002);
+			PrintWriter out = new PrintWriter(destSocket.getOutputStream(), true);
+			str = "A"+ "::" + "ALIVE";
+			out.println(str);
+			out.close();
+			destSocket.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public final void testFDsensTopologyString() {
+		try {
+			Thread.sleep(default_timeout+5000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		Vector<String> expected = new Vector<String>();
+		expected.add("B");
+		expected.add("C");
+		assertEquals(expected, sensor.getOutput());
+	}
+
+}
